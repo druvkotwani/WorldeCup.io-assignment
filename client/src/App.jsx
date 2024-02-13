7e1e0114b81af07e4d65e3ac0cbdb343ddc709d3
druvkotwani <druvkotwani12@gmail.com>
2024-02-13 17:24:31 +0530

    ChatBoxReceiver component completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { Avatar } from "@nextui-org/react";
+import ChatBoxReciever from "./components/ChatBox";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+
+      {/* <div className="flex flex-col items-center justify-center h-screen">
+        <Avatar size="xl" src="https://avatars.dicebear.com/api/avataaars/1234.svg" />
+        <h1 className="text-4xl font-bold">WordleCup.io ChatApp Assignment</h1>
+        <p className="text-lg mt-4">Created with 💗 by Dhruv</p>
+      </div> */}
+
+      <ChatBoxReciever avatar="https://i.pravatar.cc/150?u=a04258a2462d826712d" user="Dhruv" message="Hello" />
+
+    </>
   )
 }
--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Avatar } from '@nextui-org/react'
 
-const ChatBox = () => {
+export default function ChatBoxReciever({ avatar, user, message }) {
     return (
-        <div>ChatBox</div>
+        <div className='flex flex-row justify-start  ' >
+            <Avatar isBordered src={avatar} radius="sm" />
+
+            <p className='mx-2 p-4 bg-[#dcf8c6] rounded-lg max-w-60 '  >
+                <strong className='text-base  ' >
+                    {user}
+                </strong> <br></br>
+                {message}
+            </p>
+
+        </div>
     )
 }
-
-export default ChatBox
