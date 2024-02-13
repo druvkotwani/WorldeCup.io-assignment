8a0bb17e9182bdf7e171a6031a5e54db71150b87
druvkotwani <druvkotwani12@gmail.com>
2024-02-13 20:58:16 +0530

    added socket.io client

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
-
+import socketIOClient from "socket.io-client";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import ChatBoxReciever, { ChatBoxSender } from "./ChatBox";
+import InputText from "./InputText";
 const ChatContainer = () => {
+    let socketio = socketIOClient('http://localhost:3000');
+    const [chats, setChats] = useState([]);
+    const [user, setUser] = useState(localStorage.getItem('name'))
+    const [avatar, setAvatar] = useState(localStorage.getItem('avatar'))
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        socketio.on('chat', (msg) => {
+            setChats([...chats, msg])
+        })
+    })
+    function sendChatToSocket(chat) {
+        socketio.emit('chat', chat)
+    }
+
+    function addMessage(chat) {
+        const newChat = { ...chat, user, avatar }
+        setChats([...chats, newChat])
+        sendChatToSocket(newChat)
+    }
+
+    function logout() {
+        localStorage.removeItem('name')
+        localStorage.removeItem('avatar')
+        setUser('')
+        navigate('/')
+    }
+    function ChatsLists() {
+        return chats.map((chat, index) => {
+            if (chat.user === user) {
+                return <ChatBoxSender key={index} message={chat.message} avatar={chat.avatar} />
+            } else {
+                return <ChatBoxReciever key={index} message={chat.message} avatar={chat.avatar} />
+            }
+        })
+    }
     return (
-        <div>ChatContainer</div>
+        <div className='flex flex-col h-screen'>
+            <div className='flex-1 overflow-y-auto'>
+                <div style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between' }} >
+                    <h4>Username: {user}</h4>
+                    <p onClick={() => logout()} style={{ color: "blue", cursor: 'pointer' }} >Log Out</p>
+                </div>
+                <ChatsLists />
+
+                <InputText className='fixed bottom-0 left-0 right-0' addMessage={addMessage} /> {/* Apply fixed positioning */}
+            </div>
+        </div>
     )
 }
 
--- client/src/components/ChatHome.jsx
+++ client/src/components/ChatHome.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import ChatBoxReciever, { ChatBoxSender } from './ChatBox';
-import InputText from './InputText';
+import ChatContainer from './ChatContainer';
 
 const ChatHome = () => {
     return (
-        <div className='flex flex-col h-screen'>
-            <div className='flex-1 overflow-y-auto'>
-                <ChatBoxReciever avatar="https://i.pravatar.cc/150?u=a04258a2462d826712d" user="Dhruv" message="Hello" />
-                <ChatBoxSender avatar="https://i.pravatar.cc/150?u=a04258h2462d829712d" user="Dhruv" message="Hello I'm Dhruv, doing this assignment" />
-                <ChatBoxReciever avatar="https://i.pravatar.cc/150?u=a04258a2462d826712d" user="Kotwani" message="Hello" />
-                <ChatBoxSender avatar="https://i.pravatar.cc/150?u=a04258h2432d826712d" user="Unkonw" message="Hello I'm Dhruv, doing this assignment" />
-            </div>
-            <InputText className='fixed bottom-0 left-0 right-0' addMessage={(message) => message.message} /> {/* Apply fixed positioning */}
+        <div className='h-screen'>
+            <ChatContainer />
         </div>
     );
 }
