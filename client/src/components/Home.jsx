1f317c0d54b47424b2f06c49853041da5a136244
druvkotwani <druvkotwani12@gmail.com>
2024-02-13 18:41:36 +0530

    form controlled

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,21 @@
 import { Avatar } from "@nextui-org/react";
 import { Input, Button } from "@nextui-org/react";
 import { UserIcon } from "../utils/Icons";
-
+import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+    const [name, setName] = useState('');
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setName('');
+
+    };
     const backgroundImageUrl = "https://wordlecup.io/static/media/greybackground.40cdbc3f043f9a9c9357.webp";
 
     return (
@@ -13,10 +25,11 @@ export default function Home() {
                 <h1 className="sm:text-4xl text-white font-bold text-xl">WordleCup.io ChatApp Assignment</h1>
 
                 <div className="flex sm:flex-row flex-col gap-2 items-center justify-center my-4 w-full" >
-                    <form className="w-4/6 mx-2">
-                        <Input radius='sm' type="name" label="Name" />
+
+                    <form value={name} className="w-4/6 mx-2" onSubmit={(e) => handleSubmit(e)}>
+                        <Input radius='sm' type="name" label="Name" value={name} onChange={handleChange} />
                     </form>
-                    <Button color="primary" radius='sm' startContent={<UserIcon />} >
+                    <Button onClick={handleSubmit} color="primary" radius='sm' startContent={<UserIcon />} >
                         Login
                     </Button>
                 </div>
