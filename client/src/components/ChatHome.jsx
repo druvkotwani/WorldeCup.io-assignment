9fb1f9160bb49f845db36cfa4a1de0fc7bba8881
druvkotwani <druvkotwani12@gmail.com>
2024-02-14 14:55:02 +0530

    adjusted width and added icon in usericon

--- client/src/components/ChatHome.jsx
+++ client/src/components/ChatHome.jsx
@@ -3,12 +3,14 @@ import ChatContainer from './ChatContainer';
 
 const ChatHome = () => {
     return (
-        <div className='h-full w-full sm:w-4/6 mx-auto  m-4 '>
+        <div className='h-full w-full sm:w-5/6 lg:w-4/6 mx-auto  m-4 '>
             <div className='h-16 bg-[#F6FAFE] flex items-center justify-center'>
                 <h1 className='text-2xl font-bold'>Chat App</h1>
             </div>
+            <div>
 
-            <ChatContainer />
+                <ChatContainer />
+            </div>
         </div>
     );
 }
