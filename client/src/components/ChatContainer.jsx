78f4df27357e6a098a3d688e282f6f11f09603f9
druvkotwani <druvkotwani12@gmail.com>
2024-02-14 14:55:19 +0530

    user getting added into the userSide when loggin in

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -4,27 +4,28 @@ import { useNavigate } from 'react-router-dom'
 import ChatBoxReciever, { ChatBoxSender } from "./ChatBox";
 import InputText from "./InputText";
 import { Avatar, Badge, Button } from "@nextui-org/react";
-import { LogoutIcon } from "../utils/Icons";
+import { LogoutIcon, UsersIcon } from "../utils/Icons";
 const ChatContainer = () => {
-    // let socketio = socketIOClient('http://localhost:3000');
+    let socketio = socketIOClient('http://localhost:3000');
     const [chats, setChats] = useState([
-        { user: 'Kotwani', message: 'This assignment is done by Dhruv', avatar: '/images/4.png' },
-        { user: 'Dhruv', message: 'These are hardcoded text', avatar: '/images/5.svg' },
+        { user: 'Test[Bot]', message: 'This assignment is done by Dhruv', avatar: '/images/4.png' },
+        { user: 'Dhruv[Bot]', message: 'These are hardcoded text', avatar: '/images/5.svg' },
         { user: 'Bhawna', message: 'How are you?', avatar: '/images/1.png' },
     ]);
     const [user, setUser] = useState(localStorage.getItem('name'))
     const [avatar, setAvatar] = useState(localStorage.getItem('avatar'))
-
     const navigate = useNavigate()
 
-    // useEffect(() => {
-    //     socketio.on('chat', (msg) => {
-    //         setChats([...chats, msg])
-    //     })
-    // })
+    useEffect(() => {
+        socketio.on('chat', (msg) => {
+            setChats([...chats, msg])
+        })
+    })
+
+
 
     function sendChatToSocket(chat) {
-        // socketio.emit('chat', chat)
+        socketio.emit('chat', chat)
     }
 
     function addMessage(chat) {
@@ -58,36 +59,94 @@ const ChatContainer = () => {
     }, [chats]);
 
     return (
-        <>
-            <div className="flex flex-row justify-between p-2  ">
-                <div className=" flex gap-2 items-center">
-                    <Badge content="" color="success" shape="circle" placement="bottom-right">
-                        <Avatar
-                            radius="full"
-                            src='/images/1.png'
-                        />
-                    </Badge>
-                    <div className="">
-                        <p className="text-base font-bold">{user}</p>
-                        <p className="text-xs text-gray-500">I 💖 Coding</p>
+        <div className="flex items-start  justify-center bg-[#fef9fa]  border-t ">
+            <TotalUsers chats={chats} />
+            <div className="sm:w-3/4 sm:border-l-1 sm:px-2">
+                <div className="flex flex-row justify-between p-2  ">
+                    <div className=" flex gap-2 items-center">
+                        <Badge content="" color="success" shape="circle" placement="bottom-right">
+                            <Avatar
+                                radius="full"
+                                src='/images/1.png'
+                            />
+                        </Badge>
+                        <div className="">
+                            <p className="text-base font-bold">{user}</p>
+                            <p className="text-xs text-gray-500">I 💖 Coding</p>
+                        </div>
                     </div>
+
+                    <Button onClick={() => logout()} className="flex items-center justify-center" radius="sm" variant="flat"  >
+                        <LogoutIcon className="mr-2" />
+                        Logout
+                    </Button>
                 </div>
 
-                <Button onClick={() => logout()} className="flex items-center justify-center" radius="sm" variant="flat"  >
-                    <LogoutIcon className="mr-2" />
-                    Logout
-                </Button>
+                <div className="flex-grow overflow-y-auto  h-[calc(77vh-3rem)]  hide-scrollbar  " ref={chatListRef}>
+                    <ChatsLists />
+                </div>
+
+                <div className="fixed bottom-0 left-0 right-0 sm:w-4/6 sm:mx-auto z-10">
+                    <InputText addMessage={addMessage} />
+                </div>
             </div>
 
-            <div className="flex-grow overflow-y-auto  h-[calc(77vh-3rem)]  hide-scrollbar  " ref={chatListRef}>
-                <ChatsLists />
-            </div>
+        </div>
+    )
+}
+export default ChatContainer
 
-            <div className="fixed bottom-0 left-0 right-0 sm:w-4/6 sm:mx-auto z-10">
-                <InputText addMessage={addMessage} />
-            </div>
-        </>
+const TotalUsers = ({ chats }) => {
+
+    const uniqueUsersMap = {};
+
+    const uniqueUsers = chats.reduce((acc, chat) => {
+        if (!acc.some(user => user.user === chat.user)) {
+            acc.push(chat);
+        }
+        return acc;
+    }, []);
+
+    uniqueUsers.push({
+        user: localStorage.getItem('name'),
+        avatar: localStorage.getItem('avatar')
+    });
+
+    uniqueUsers.forEach(user => {
+        if (!uniqueUsersMap[user.user]) {
+            uniqueUsersMap[user.user] = user;
+        }
+    });
+
+    const onlyUsers = Object.values(uniqueUsersMap).map(user => ({
+        name: user.user,
+        avatar: user.avatar
+    }));
+
+    console.log(onlyUsers)
+    return (
+        <div className="w-1/4 hidden sm:block ">
+            <h1 className="text-2xl font-bold flex items-center justify-center gap-2 border-b-1 py-2">
+                <UsersIcon />
+                Users
+            </h1>
+            <ul className="mx-auto">
+                {
+                    onlyUsers.map((user, index) => {
+
+                        return (
+                            <li key={index} className="flex items-center justify-start gap-2 p-2 border-b-1">
+                                <Avatar
+                                    radius="full"
+                                    src={user.avatar}
+                                />
+                                <p className="text-base font-medium">{user.name}</p>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
-export default ChatContainer
