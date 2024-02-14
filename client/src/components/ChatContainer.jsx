3adee05d074e4663ccd2ef00ba6e679ae209f1b5
druvkotwani <druvkotwani12@gmail.com>
2024-02-14 13:11:50 +0530

    images added, font changed, scrollbar functionality

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -6,21 +6,25 @@ import InputText from "./InputText";
 import { Avatar, Badge, Button } from "@nextui-org/react";
 import { LogoutIcon } from "../utils/Icons";
 const ChatContainer = () => {
-    let socketio = socketIOClient('http://localhost:3000');
-    const [chats, setChats] = useState([]);
+    // let socketio = socketIOClient('http://localhost:3000');
+    const [chats, setChats] = useState([
+        { user: 'Kotwani', message: 'This assignment is done by Dhruv', avatar: '/images/4.png' },
+        { user: 'Dhruv', message: 'These are hardcoded text', avatar: '/images/5.svg' },
+        { user: 'Bhawna', message: 'How are you?', avatar: '/images/1.png' },
+    ]);
     const [user, setUser] = useState(localStorage.getItem('name'))
     const [avatar, setAvatar] = useState(localStorage.getItem('avatar'))
 
     const navigate = useNavigate()
 
-    useEffect(() => {
-        socketio.on('chat', (msg) => {
-            setChats([...chats, msg])
-        })
-    })
+    // useEffect(() => {
+    //     socketio.on('chat', (msg) => {
+    //         setChats([...chats, msg])
+    //     })
+    // })
 
     function sendChatToSocket(chat) {
-        socketio.emit('chat', chat)
+        // socketio.emit('chat', chat)
     }
 
     function addMessage(chat) {
@@ -44,33 +48,37 @@ const ChatContainer = () => {
             }
         })
     }
+
     return (
-        <div className='flex flex-col h-screen'>
-            <div className=' flex-1 overflow-y-auto '>
-                <div className="flex flex-row justify-between p-2 border-b bg-[#F6FAFE]">
-                    <div className="flex gap-2 items-center">
-                        <Badge content="" color="success" shape="circle" placement="bottom-right">
-                            <Avatar
-                                radius="full"
-                                src={avatar}
-                            />
-                        </Badge>
-                        <div className="">
-                            <p className="text-base font-bold">{user}</p>
-                            <p className="text-xs text-gray-500">I 💖 Coding</p>
-                        </div>
+        <>
+            <div className="flex flex-row justify-between p-2  ">
+                <div className=" flex gap-2 items-center">
+                    <Badge content="" color="success" shape="circle" placement="bottom-right">
+                        <Avatar
+                            radius="full"
+                            src='/images/1.png'
+                        />
+                    </Badge>
+                    <div className="">
+                        <p className="text-base font-bold">{user}</p>
+                        <p className="text-xs text-gray-500">I 💖 Coding</p>
                     </div>
-
-                    <Button onClick={() => logout()} className="flex items-center justify-center" radius="sm" variant="flat"  >
-                        <LogoutIcon className="mr-2" />
-                        Logout
-                    </Button>
                 </div>
-                <ChatsLists />
 
-                <InputText className='fixed bottom-0 left-0 right-0' addMessage={addMessage} /> {/* Apply fixed positioning */}
+                <Button onClick={() => logout()} className="flex items-center justify-center" radius="sm" variant="flat"  >
+                    <LogoutIcon className="mr-2" />
+                    Logout
+                </Button>
             </div>
-        </div>
+
+            <div className="flex-grow overflow-y-auto  h-[calc(70vh-1rem)]  hide-scrollbar ">
+                <ChatsLists />
+            </div>
+
+            <div className="fixed bottom-0 left-0 right-0 sm:w-4/6 sm:mx-auto z-10 ">
+                <InputText addMessage={addMessage} />
+            </div>
+        </>
     )
 }
 
--- client/src/components/ChatHome.jsx
+++ client/src/components/ChatHome.jsx
@@ -3,7 +3,11 @@ import ChatContainer from './ChatContainer';
 
 const ChatHome = () => {
     return (
-        <div className='h-screen'>
+        <div className='h-full w-full sm:w-4/6 mx-auto border-x-1 boder-y-1    m-4 '>
+            <div className='h-16 bg-[#F6FAFE] flex items-center justify-center'>
+                <h1 className='text-2xl font-bold'>Chat App</h1>
+            </div>
+
             <ChatContainer />
         </div>
     );
