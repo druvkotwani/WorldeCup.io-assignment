a7582d2e84fac6b509df39a97237fd044f525be6
druvkotwani <druvkotwani12@gmail.com>
2024-02-14 16:51:29 +0530

    added logout button which was earlier in ChatContainer

--- client/src/components/ChatHome.jsx
+++ client/src/components/ChatHome.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
 import ChatContainer from './ChatContainer';
-import { ChatIcon } from '../utils/Icons';
+import { ChatIcon, LogoutIcon } from '../utils/Icons';
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom'
 
 const ChatHome = () => {
+    const navigate = useNavigate()
+
+    function logout() {
+        localStorage.removeItem('name')
+        localStorage.removeItem('avatar')
+        // setUser('')
+        navigate('/')
+    }
     return (
-        <div className='h-full w-full sm:w-5/6 lg:w-4/6 mx-auto  m-4 '>
-            <div className='h-16 bg-[#F6FAFE] flex items-center justify-center'>
-                <h1 className='text-2xl gap-2 presStart flex items-center justify-center'>
+        <div className='h-full w-full sm:w-5/6 lg:w-4/6 mx-auto mt-4'>
+            <div className='h-16 bg-[#F6FAFE] flex items-center justify-between px-6 '>
+                <h1 className='sm:text-2xl text-lg  gap-2 presStart flex items-center justify-center'>
                     <ChatIcon />
                     Chat-Room
                 </h1>
-
+                <Button onClick={() => logout()} className="flex items-center justify-center min-w-6 sm:min-w-6  " radius="sm" variant="flat"  >
+                    <LogoutIcon className="mr-2" />
+                    <span className='hidden sm:block'>Leave Room</span>
+                </Button>
             </div>
             <div>
 
